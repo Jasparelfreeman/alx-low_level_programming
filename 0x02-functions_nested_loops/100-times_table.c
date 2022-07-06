a96340d245c42a67dbbf6afec77f12b64c1292c6
Jasparelfreeman <talk2adegun@yahoo.com>
2022-07-05 20:58:59 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,10 +3,10 @@
  * print_times_table - prints times table
  * @n : times table to use
  * Return:void
- */ 
-  
-void print_times_table(int n) 
-{ 
+ */
+
+void print_times_table(int n)
+{
 
 	int a = 0, rep, b;
 
