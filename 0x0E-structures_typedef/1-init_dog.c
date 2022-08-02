f75082aef2221010bc05f39402e2612d4bc10282
Jasparelfreeman <talk2adegun@yahoo.com>
2022-08-01 21:58:10 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* init_dog - Initialize a variable type struct dog
+* @d: First argument (pointer to a structure);
+* @name: Second argument (name of the dog)
+* @age: Third argument (age of the dog)
+* @owner: Fourth argument (owner of the dog)
+* Return: NOTHING
+*/
