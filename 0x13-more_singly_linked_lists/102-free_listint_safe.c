0ee3ae6ddc6257b279b6c558daa05d692c104c94
Jasparelfreeman <talk2adegun@yahoo.com>
2022-08-15 16:59:35 -0700

    Task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,32 +1,65 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
-* find_listint_loop - finds the loop in a linked list.
-* @head: pointer to the beginning of the list
+* _ra - reallocates memory for an array of pointers
+* to the nodes in a linked list
+* @list: the old list to append
+* @size: size of the new list (always one more than the old list)
+* @new: new node to add to the list
 *
-* Return: address of the node where the loop starts or NULL if there's no loop
+* Return: pointer to the new list
 */
-listint_t *find_listint_loop(listint_t *head)
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-listint_t *tortoise, *hare;
+listint_t **newlist;
+size_t i;
 
-tortoise = hare = head;
-while (tortoise && hare && hare->next)
+newlist = malloc(size * sizeof(listint_t *));
+if (newlist == NULL)
 {
-tortoise = tortoise->next;
-hare = hare->next->next;
-if (tortoise == hare)
+free(list);
+exit(98);
+}
+for (i = 0; i < size - 1; i++)
+newlist[i] = list[i];
+newlist[i] = new;
+free(list);
+return (newlist);
+}
+
+/**
+* free_listint_safe - frees a listint_t linked list.
+* @head: double pointer to the start of the list
+*
+* Return: the number of nodes in the list
+*/
+size_t free_listint_safe(listint_t **head)
 {
-tortoise = head;
-break;
+size_t i, num = 0;
+listint_t **list = NULL;
+listint_t *next;
+
+if (head == NULL || *head == NULL)
+return (num);
+while (*head != NULL)
+{
+for (i = 0; i < num; i++)
+{
+if (*head == list[i])
+{
+*head = NULL;
+free(list);
+return (num);
 }
 }
-if (!tortoise || !hare || !hare->next)
-return (NULL);
-while (tortoise != hare)
-{
-tortoise = tortoise->next;
-hare = hare->next;
+num++;
+list = _ra(list, num, *head);
+next = (*head)->next;
+free(*head);
+*head = next;
 }
-return (hare);
+free(list);
+return (num);
 }
